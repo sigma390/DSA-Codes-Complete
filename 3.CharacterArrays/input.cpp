3b7683090ca5d36541ff683d1563d4d04debecdd
Omkar Patil <git21cs072-t@students.git.edu>
2023-11-25 08:18:15 +0530

    Input Store and Print output

--- 3.CharacterArrays/input.cpp
+++ 3.CharacterArrays/input.cpp
@@ -4,21 +4,24 @@ using namespace std;
 
 
 int main(){
-    // char a[10];
+    char para[1000];
     // char b[20];
     // cin>>a; // cant read white spaces
     // cout<<a;
     //=======> solutions <============
-    char temp = cin.get(); //input buffer
+    char temp = cin.get(); //input buffer  
+
+
     int len{1};
-    while(temp!='\n'){
-        len++;
-        cout<<temp;
+    //input as well as storing
+    while(temp!='#'){
+        para[len++] = temp;
         temp = cin.get();
     }
     
 
    cout<<"\nlength is :"<<len;
+   cout<<para<<endl;
     
     return 0;
 }
