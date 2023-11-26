604c4d38ec1dae915fcfd3d2fd5f44cc87d1ffd9
Omkar Patil <git21cs072-t@students.git.edu>
2023-11-26 18:52:24 +0530

    Shortest Path Problem Done

--- 3.CharacterArrays/Shortestpath.cpp
+++ 3.CharacterArrays/Shortestpath.cpp
@@ -10,7 +10,6 @@ int main(){
         cin.getline(route,1000);
         int x{0};
         int y{0};
-        
         for (int i = 0; route[i]!= '\0'; i++)
         {
             switch (route[i])
@@ -28,12 +27,59 @@ int main(){
                 
                 break;
 
-            
             default:
                 break;
             }
         }
         cout<<x<<","<<y<<endl;
-    
+        //1ST QUADRANT
+        if (x>=0 and y>=0)
+        {
+            while(y--){
+                cout<<'S';
+
+            }
+            while (x--)
+            {
+                cout<<'E';
+            }
+        } 
+        // 4TH QUAD
+        else if (x>=0 and y< 0)
+        {
+            while(y++){
+                cout<<'N';
+
+            }
+            while (x--)
+            {
+                cout<<'E';
+            }
+        }
+        //2ND QUAD
+        else if (x<0 and y>=0)
+        {
+            while(y--){
+                cout<<'S';
+
+            }
+            while (x++)
+            {
+                cout<<'W';
+            }
+        }
+        //3RD QUAD
+        else if (x<0 and y<0)
+        {
+            while(y++){
+                cout<<'N';
+
+            }
+            while (x++)
+            {
+                cout<<'W';
+            }
+        }
+        
     return 0;
 }
