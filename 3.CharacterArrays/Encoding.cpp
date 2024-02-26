e5fad31dca870e9aaa489a4bdf4e50283033fa14
Omkar Patil <git21cs072-t@students.git.edu>
2024-02-26 19:19:00 +0530

    Done String Encoding Problem!!!

---
+++ 3.CharacterArrays/Encoding.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string Encoding(string str){
+    int count{0};
+    string s;
+    int n =str.length();
+
+    for (int i = 0; i < n; i++)
+    {
+        int count = 1;
+        while (i< n-1 and str[i]==str[i+1] )
+        {
+            count++;
+            i++;
+        }
+        s= s+ str[i]+to_string(count);
+        
+    }
+
+
+    if (s.length()>str.length())
+    {
+        return str;
+
+    }
+    return s;
+    
+    
+    
+
+
+
+
+
+
+
+};
+
+int main(){
+
+    string s = "aaabbcccdd";
+    string a = Encoding(s);
+    cout<<a<<endl;
+
+    return 0;
+}
