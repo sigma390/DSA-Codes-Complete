a4b61df000f6d76b043d7f805d67aee8da3904c1
Omkar Patil <sigmacoder390.gmail.com>
2024-03-10 23:32:14 +0530

    Done Char 2d array!!!

---
+++ 4.2D arrays/String2darray.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    char nums[][10] = {
+        "one", // each word is a row whre the end is '\n'
+        "two",
+        "three",
+        "four",
+        "five"
+    };
+    // to print and clarify
+    int i= 2;
+    cout<<nums[i]<<endl;
+    // to print individual letter 
+    cout <<nums[i][1];
+
+
+
+    return 0;
+
+}
+
