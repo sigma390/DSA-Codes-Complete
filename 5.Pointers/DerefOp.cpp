3e6e907c446e298f741c77039d1837923f6d6658
Omkar Patil <git21cs072-t@students.git.edu>
2024-03-20 21:42:27 +0530

    deref op done!!

---
+++ 5.Pointers/DerefOp.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace  std;
+
+int main(){
+
+
+    int x = 10;
+    int *ptr  = &x;
+    cout<<ptr<<endl;
+    cout<<x<<endl;
+    cout<<*ptr<<endl;
+
+
+
+
+    return 0;
+}
