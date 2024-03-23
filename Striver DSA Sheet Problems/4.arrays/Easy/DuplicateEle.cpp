74d1d50d79d20f500f4b167e743e0db7ec28661b
Omkar Patil <git21cs072-t@students.git.edu>
2024-03-23 22:58:05 +0530

    Duplicate wala done !!!

---
+++ Striver DSA Sheet Problems/4.arrays/Easy/DuplicateEle.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int isDuplicate(vector<int> &arr, int n) {
+    if (n == 0) // handle empty vector
+        return 0;
+
+    int i = 0;
+    for (auto it = arr.begin() + 1; it != arr.end(); it++) {
+        if (arr[i] != *it) {
+            i++;
+            arr[i] = *it; 
+        }
+    }
+    return i + 1; 
+}
+
+int main() {
+    vector<int> arr = {0, 11, 22, 22, 33, 44, 44, 44, 44}; //expected  4
+    int n = arr.size();
+    int r = isDuplicate(arr, n);
+    cout << r << endl;
+
+    for (int i = 0; i < r; i++) {
+        cout << arr[i] << ",";
+    }
+
+    return 0;
+}
