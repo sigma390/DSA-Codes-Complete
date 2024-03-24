719936dc5511917c8b07973af253ee887869d6bc
Omkar Patil <git21cs072-t@students.git.edu>
2024-03-24 12:40:04 +0530

    doing it!!!

---
+++ Striver DSA Sheet Problems/4.arrays/Easy/ritattewithsteps.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int> rotateArray(vector<int>& arr, int n, int k) {
+    // Write your code here.
+
+    vector<int>temp;
+    for (int i = d; i < ; i++)
+    {
+        /* code */
+    }
+    
+    
+   
+
+    
+}
+
+
+
+
+int main(){
+
+    vector<int> arr = {1,2,3,4,5,6,7};
+    int n = arr.size();
+
+    rotateArray(arr,n,3);
+    for (auto &&i : arr)
+    {
+        cout<<i<<",";
+    }
+    
+
+
+
+
+
+
+
+    return 0;
+}
