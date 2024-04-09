7582c44016143b2e4976ccdd21077699a4768151
Omkar Patil <git21cs072-t@students.git.edu>
2024-04-09 21:30:29 +0530

    factorial learnt!!

---
+++ 10.Recursion/factorial.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fact(int n){
+    //base case 
+    if (n==0)
+    {
+        return 1;
+    }
+    return n*fact(n-1);
+    
+}
+
+
+
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<fact(n);
+
+
+    return 0;
+}
