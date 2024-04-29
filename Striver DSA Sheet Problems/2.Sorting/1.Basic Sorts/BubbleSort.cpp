b642db6d17affdd28433ef25d2fd1058d2f5d438
Omkar Patil <git21cs072-t@students.git.edu>
2024-04-29 19:15:07 +0530

    Bubble sort done!!!

--- Striver DSA Sheet Problems/2.Sorting/1.Basic Sorts/BubbleSort.cpp
+++ Striver DSA Sheet Problems/2.Sorting/1.Basic Sorts/BubbleSort.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void bsort(int arr[],int n){
-    for (int times = 0; times < n-1; times++)
+void Bubble(int arr[],int n){
+    
+    
+    for (int i = 0; i < n-1; i++)
     {
-        for (int j = 0; j < n-times -1; j++)
+        
+        for (int j = 0; j < n-i-1; j++)
         {
             if (arr[j]>arr[j+1])
             {
@@ -14,21 +17,21 @@ void bsort(int arr[],int n){
         }
         
     }
-    
+
 }
 
 
 int main(){
 
+    // unsorted array
     int arr[] = {6,7,1,2,9,5,3};
     int n = sizeof(arr)/sizeof(int);
-    bsort(arr,n);
 
-    for (auto x : arr)
+    Bubble(arr,n);
+    for (auto x: arr)
     {
-        cout<<x<<',';
+        cout<<x<<",";
     }
     
     return 0;
-
 }
