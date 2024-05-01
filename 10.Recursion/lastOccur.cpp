70b53e939fb13e1323f93dc4bd9bd5ecffd4185e
Omkar Patil <git21cs072-t@students.git.edu>
2024-05-01 19:03:52 +0530

    last Occurance Learnt!!!

--- 10.Recursion/lastOccur.cpp
+++ 10.Recursion/lastOccur.cpp
@@ -4,13 +4,36 @@ using namespace std;
 
 int lastOcc(int arr[] ,  int n , int k)
 {
+    if(n==0){
+        return -1;
+
+    }
+    int sub = lastOcc(arr+1, n-1, k);
+    if (sub==-1)
+    {
+        if (k==arr[0])
+        {
+            return 0;
+        }
+        else{
+            return -1;
+        }
+        
+    }
+    else{
+        return sub+1;
+    }
+    
 
 }
 
 
 int main(){
 
-    int arr [] = {1,2,3,4,5,4}
+    int arr [] = {1,2,3,4,5,4,7,8,9};
+    int n = sizeof(arr)/sizeof(int);
+    cout<<lastOcc(arr,n,4);
+    
 
     return 0;
 }
