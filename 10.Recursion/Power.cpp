f2376d36ffd43afd3956fae19b9d94dd259d19b9
Omkar Patil <git21cs072-t@students.git.edu>
2024-05-01 21:18:09 +0530

    learnt Optimised power Method 1!!!!

--- 10.Recursion/Power.cpp
+++ 10.Recursion/Power.cpp
@@ -10,7 +10,22 @@ long long Power(int a , int n){
     
 }
 
+long long FasterPower(int a, int n){
+    if (n==0){
+        return 1;}
 
+    int subPrblm = FasterPower(a, n/2);
+    int SubSquare = subPrblm*subPrblm;
+    if(n&1){
+        return a*SubSquare;
+    }
+    else{
+        return SubSquare;
+    }
+
+    
+    
+}
 
 
 
@@ -18,7 +33,8 @@ int main(){
     
     int a {0};
     cin>>a;
-    cout<<Power(a, 5);
+    cout<<Power(a, 5)<<endl;
+    cout<<FasterPower(a,10);
  
     return 0;
 }
