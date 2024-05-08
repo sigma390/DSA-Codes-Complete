373f405aadc83a437f1ccf17cf4b8a9147e12231
Omkar Patil <git21cs072-t@students.git.edu>
2024-05-08 18:10:45 +0530

    No of possible gridways

---
+++ 12.Backtracking/GridWAYS.cpp
@@ -0,0 +1,32 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int gridways(int m, int n , int rows , int columns){
+    //base case 
+    if(rows==m-1 and columns ==n-1){
+        return 1;
+    }
+    //corner case
+    if (rows>=m or columns>=n)
+    {
+        return 0;
+
+    }
+
+    int ans{0};
+    ans = gridways(m,n,rows+1,columns)+gridways(m,n,rows,columns+1);
+    return ans;
+    
+
+}
+
+int main(){
+    int m, n;
+    cin>>m>>n;
+    cout<<gridways(m,n,0,0);
+    return 0;
+}
