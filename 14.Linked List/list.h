9b499bd1d9f6c96f5142c123475955895b77b1fe
Omkar Patil <git21cs072-t@students.git.edu>
2024-05-12 09:28:28 +0530

    Push back and Push Front Implemented

--- 14.Linked List/list.h
+++ 14.Linked List/list.h
@@ -1,20 +1,58 @@
-#include <cstddef>
-
-
-class Node
-{
+#include <iostream>
+class List;
+class Node {
     int data;
-    Node* next;
-public:
-    Node(int d):data(d),next(NULL){}
-
-};
-
-class List{
-    Node* head;
-    Node* tail;
-public:
-    List():head(NULL),tail(NULL){}
     
 
+public:
+    Node* next;
+    Node(int d) : data(d), next(nullptr) {}
+    int getData(){
+        return data;
+    }
+
+    friend class List;
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() : head(nullptr), tail(nullptr) {}
+    Node *begin(){
+        return head;
+    }
+    ~List() {
+        while (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    // Push front
+    void push_front(int data) {
+        if (head == nullptr) {
+            Node* n = new Node(data);
+            head = tail = n;
+        } else {
+            Node* n = new Node(data);
+            n->next = head;
+            head = n;
+        }
+    }
+
+
+    //push back 
+    void pus_back(int data){
+        if (head == nullptr) {
+            Node* n = new Node(data);
+            head = tail = n;}
+        else{
+            Node * n = new Node(data);
+            tail->next  = n;
+            tail = n;
+        }
+    }
 };
