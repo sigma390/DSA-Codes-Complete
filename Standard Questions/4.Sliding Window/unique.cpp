ef439cb91bfd33da96557cc4baa0141d2b431787
Omkar Patil <git21cs072-t@students.git.edu>
2024-06-04 14:03:40 +0530

    Unique string done!!!

---
+++ Standard Questions/4.Sliding Window/unique.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string unk(string s){
+    //two pointers
+    int j = 0;
+    int i = 0;
+    //map 
+    unordered_map<char, int> res;
+    int win_len = 0;
+    int max_win_len = 0;
+    int start_win = -1;
+
+    //traverse
+    while(j<s.length()){
+        char ch = s[j];
+        //check its there in hashmap or not and its idx is > start of curent window
+        if(res.count(ch) and res[ch]>=i){
+            i = res[ch]+1;
+            win_len = j-i; //updated Window Length Excluding current char 
+
+        }
+
+        //update last occuranvce
+        res[ch] = j;
+        win_len++;
+        j++;
+
+        //update amx_window length
+        if (win_len>max_win_len)
+        {
+            max_win_len = win_len;
+            start_win = i;
+        }
+        
+
+    }
+    return s.substr (start_win, max_win_len);
+
+}
+
+int main(){
+    string s = "abcabed";
+    cout<<unk(s);
+    return 0;
+}
