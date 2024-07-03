66937f654f9addece3614a508429044a49325ced
Omkar Patil <git21cs072-t@students.git.edu>
2024-07-03 17:40:10 +0530

    inorder and Postorder Done!!!

--- 17.Trees/Preorder.cpp
+++ 17.Trees/Preorder.cpp
@@ -52,12 +52,34 @@ void printPreorder(Node *root){
     
 }
 
+void printInorder(Node *root){
+    if(root==NULL)return;
+    printInorder(root->left);
+    cout<<root->data<<" ";
+    printInorder(root->right);
+}
+void printPostorder(Node *root){
+    if(root==NULL)return;
+    printInorder(root->left);
+    
+    printInorder(root->right);
+    cout<<root->data<<" ";
+}
+
+
+
+
 int main()
 {
     //Build tree
     Node * root = buildTree();
     //print
+    cout<<"PreOrder is :"<<endl;
     printPreorder(root);
+    cout<<"\nInOrder is :"<<endl;
+    printInorder(root);
+    cout<<"\nPostOrder is :"<<endl;
+    printPostorder(root);
 
 
     return 0;
