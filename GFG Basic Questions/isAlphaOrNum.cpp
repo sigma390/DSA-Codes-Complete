cf0f099204da192db2ee64e62d44f284f2fc1c6f
Omkar Patil <git21cs072-t@students.git.edu>
2024-07-27 20:38:06 +0530

    is Alphabet Or not CODE DOEN!!!

---
+++ GFG Basic Questions/isAlphaOrNum.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    char x;
+    cin >> x;
+    if (isalpha(x))
+    {
+        cout << "alphabet" << endl;
+    }
+    else
+    {
+        cout << "not an alphabet" << endl;
+    }
+    return 0;
+}
