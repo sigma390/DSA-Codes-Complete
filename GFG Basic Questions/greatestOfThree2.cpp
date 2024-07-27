d7d3dd8aa0c7a60a298dfa8714943a896584bcf4
Omkar Patil <git21cs072-t@students.git.edu>
2024-07-27 18:00:05 +0530

    Done greatest fo three Code!!!!

--- GFG Basic Questions/greatestOfThree2.cpp
+++ GFG Basic Questions/greatestOfThree2.cpp
@@ -3,9 +3,19 @@ using namespace std;
 
 int main()
 {
+    int a{0}, b{0}, c{0};
+    cin >> a >> b >> c;
+    if (a > b and a > c)
+    {
+        cout << "A is Greatest" << endl;
+    }
+    else if (a < b and b > c)
+    {
+        cout << "B is Greatest" << endl;
+    }
+    else
+    {
+        cout << "C is Greatest" << endl;
+    }
     return 0;
-    ;
-    ;
-    ;
-    ;
 }
