78670d85257d9acefd9453e6e84e411faf39c338
Omkar Patil <390.ompatil@gmail.com>
2024-07-27 17:46:59 +0530

    Started GFG 100 codes Folder

---
+++ GFG Basic Questions/posneg1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin >> n;
+    if (n > 0)
+    {
+        cout << "Positive";
+    }
+    else
+    {
+        cout << "negative" << endl;
+    }
+    return 0;
+}
