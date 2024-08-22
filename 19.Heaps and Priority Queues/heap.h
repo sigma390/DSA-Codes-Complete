3a49a7f1d55937f0be26ee5e4efe41a8ec8e08e6
Omkar Patil <git21cs072-t@students.git.edu>
2024-08-22 11:53:14 +0530

    Remove and Empty Method implemented !!

--- 19.Heaps and Priority Queues/heap.h
+++ 19.Heaps and Priority Queues/heap.h
@@ -5,13 +5,46 @@ class Heap
 {
 
     vector<int> arr;
+    string heapType;
+
+    // heapify method
+    void heapify(int i)
+    {
+        int left = 2 * i;
+        int right = 2 * i + 1;
+        int min = i;
+
+        // comparison
+        if (left < arr.size() && compare(arr[left], arr[min]))
+            min = left;
+        if (right < arr.size() && compare(arr[right], arr[min]))
+        {
+            min = right;
+        }
+        while (min != i)
+        {
+            swap(arr[i], arr[min]);
+            heapify(min);
+        }
+    }
 
 public:
-    Heap(int defaultSize = 10)
+    bool compare(int a, int b)
+    {
+        if (heapType == "min")
+        {
+            return a < b;
+        }
+        else
+        {
+            return a > b; // max heap
+        }
+    }
+    Heap(int defaultSize = 10, string hType)
     {
         arr.reserve(defaultSize + 1);
         arr.push_back(-1); // Oth index is inaccessible
-
+        this->heapType = hType;
         // last element arr.size()-1
     }
     void Push(int d)
@@ -22,12 +55,35 @@ public:
         // now checking Of index and Parent
         int idx = arr.size() - 1;
         int parent = idx / 2;
-        while (idx > 1 and arr[parent] > arr[idx])
+        while (idx > 1 and compare(arr[idx], arr[parent]))
         {
             swap(arr[parent], arr[idx]);
             // change idx to parent
             idx = parent;
             parent = parent / 2;
         }
+    }
+
+    // get top most element
+    int top()
+    {
+        return arr[1];
+    }
+
+    // remove method
+    void pop()
+    {
+        // swap first and last
+        int idx = arr.size() - 1;
+        swap(arr[1], arr[idx]);
+        // remove last element
+        arr.pop_back();
+        // now heapify the root
+    }
+
+    // empty Method
+    bool isEmpty()
+    {
+        return arr.size() == 1;
     }
 };
