71859465a33975eaf51848a2f5435c88c5fab812
Omkar Patil <git21cs072-t@students.git.edu>
2024-08-25 13:53:16 +0530

    isPRIME dONE!!!

---
+++ GFG Basic Questions/primeNum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool isPrime(int n){
+    if(n==1) return 0;
+    if(n==2) return 1;
+    return n&1?1:0;
+}
+
+int main(){
+    cout<<isPrime(2)<<endl;
+    return 0;
+}
