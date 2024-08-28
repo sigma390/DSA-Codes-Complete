dc54ce096585de7ced5a268363be6e6b96221181
Omkar Patil <git21cs072-t@students.git.edu>
2024-08-28 12:07:59 +0530

    38.Rev string Recursively Done!!!

--- GFG Basic Questions/revStringRec.cpp
+++ GFG Basic Questions/revStringRec.cpp
@@ -3,7 +3,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void rev(string &s, int start, int end)
+{
+    if (start >= end)
+    {
+        return;
+    }
+
+    // swap elements
+    swap(s[start], s[end]);
+    rev(s, start + 1, end - 1);
+}
+
 int main()
 {
+    string s = "hello";
+    rev(s, 0, s.length() - 1);
+    cout << s << endl;
     return 0;
 }
