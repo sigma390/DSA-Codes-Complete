98a5c53264cfded4625d0c4026d65e569045b9f3
Omkar Patil <git21cs072-t@students.git.edu>
2024-09-20 23:29:42 +0530

    Something Written

--- 17.Trees/Preorder.cpp
+++ 17.Trees/Preorder.cpp
@@ -1,86 +1,82 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 class Node
 {
 private:
-    
 public:
     int data;
-    Node*left;
-    Node*right;
-    //constructor
-    Node(int d ){
-        data  = d;
+    Node *left;
+    Node *right;
+    // constructor
+    Node(int d)
+    {
+        data = d;
         left = NULL;
         right = NULL;
     };
-   
 };
 
-//functional ApproaCH  ]
+// functional ApproaCH  ]
 
-Node* buildTree(){
-    int d ;
-    cin>>d;
-    //if d == -1 return null
-    if (d==-1)
+Node *buildTree()
+{
+    int d;
+    cin >> d;
+    // if d == -1 return null
+    if (d == -1)
     {
         return NULL;
     }
-    
 
-
-    //create New node
+    // create New node
     Node *newNode = new Node(d);
-    //make Recursive Call on both sides 
+    // make Recursive Call on both sides
     newNode->left = buildTree();
     newNode->right = buildTree();
 
     return newNode;
 }
-void printPreorder(Node *root){
-    if (root==NULL)
+void printPreorder(Node *root)
+{
+    if (root == NULL)
     {
         return;
     }
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     printPreorder(root->left);
     printPreorder(root->right);
-
-    
 }
 
-void printInorder(Node *root){
-    if(root==NULL)return;
+void printInorder(Node *root)
+{
+    if (root == NULL)
+        return;
     printInorder(root->left);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     printInorder(root->right);
 }
-void printPostorder(Node *root){
-    if(root==NULL)return;
+void printPostorder(Node *root)
+{
+    if (root == NULL)
+        return;
     printInorder(root->left);
-    
+
     printInorder(root->right);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
 }
-
-
-
 
 int main()
 {
-    //Build tree
-    Node * root = buildTree();
-    //print
-    cout<<"PreOrder is :"<<endl;
+    // Build tree
+    Node *root = buildTree();
+    // print
+    cout << "PreOrder is :" << endl;
     printPreorder(root);
-    cout<<"\nInOrder is :"<<endl;
+    cout << "\nInOrder is :" << endl;
     printInorder(root);
-    cout<<"\nPostOrder is :"<<endl;
+    cout << "\nPostOrder is :" << endl;
     printPostorder(root);
-
 
     return 0;
 }
--- template.cpp
+++ template.cpp
@@ -1,7 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define A 10 + 10
 
-
-int main(){
-    return 0;
+int main()
+{
+<<<<<<< Tabnine <<<<<<<
+/**//+
+ * @brief The main function of the program.//+
+ *//+
+ * This function serves as the entry point for the program.//+
+ * It does not take any parameters and returns an integer.//+
+ *//+
+ * @return 0 on successful program execution.//+
+ *///+
+int main()//+
+{         //+
+    return 0; //+
+} //+
+>>>>>>> Tabnine >>>>>>>// {"conversationId":"9c095c42-269d-4d08-b750-bc21cf6a34a6","source":"instruct"}
+return 0;
 }
