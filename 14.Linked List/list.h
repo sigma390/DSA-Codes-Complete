d965300cff7719c2dbd1547da4354aa03350655a
Omkar Patil <git21cs072-t@students.git.edu>
2024-10-16 18:55:40 +0530

    list

--- 14.Linked List/list.h
+++ 14.Linked List/list.h
@@ -1,152 +1,158 @@
 #include <iostream>
 using namespace std;
 class List;
-class Node {
+class Node
+{
     int data;
-    
 
 public:
-    Node* next;
+    Node *next;
     Node(int d) : data(d), next(nullptr) {}
-    int getData(){
+    int getData()
+    {
         return data;
     }
-    //destructor for Node as Well
-    ~Node(){
-        if (next!=NULL)
+    // destructor for Node as Well
+    ~Node()
+    {
+        if (next != NULL)
         {
             delete next;
         }
-       
-        
     }
 
     friend class List;
 };
 
-class List {
-    Node* head;
-    Node* tail;
+class List
+{
+    Node *head;
+    Node *tail;
 
-
-
-    int searchHelper(Node *start, int k){
-        //base 
-        if(start==NULL){
+    int searchHelper(Node *start, int k)
+    {
+        // base
+        if (start == NULL)
+        {
             return -1;
         }
-        //normal
-        if(start->data==k){
+        // normal
+        if (start->data == k)
+        {
             return 0;
         }
-        //recursive case
+        // recursive case
         int idxx = searchHelper(start->next, k);
-        if(idxx==-1){
+        if (idxx == -1)
+        {
             return -1;
-
         }
-        else{
-            return idxx+1;
+        else
+        {
+            return idxx + 1;
         }
-
-
-
     }
 
 public:
     List() : head(nullptr), tail(nullptr) {}
-    Node *begin(){
+    Node *begin()
+    {
         return head;
     }
-    //destructor down below
-    ~List() {
-       if(head!=NULL){
-        delete head;
-        head = NULL;
-       }
+    // destructor down below
+    ~List()
+    {
+        if (head != NULL)
+        {
+            delete head;
+            head = NULL;
+        }
     }
 
     // Push front
-    void push_front(int data) {
-        if (head == nullptr) {
-            Node* n = new Node(data);
+    void push_front(int data)
+    {
+        if (head == nullptr)
+        {
+            Node *n = new Node(data);
             head = tail = n;
-        } else {
-            Node* n = new Node(data);
+        }
+        else
+        {
+            Node *n = new Node(data);
             n->next = head;
             head = n;
         }
     }
 
-
-    //push back 
-    void pus_back(int data){
-        if (head == nullptr) {
-            Node* n = new Node(data);
-            head = tail = n;}
-        else{
-            Node * n = new Node(data);
-            tail->next  = n;
+    // push back
+    void pus_back(int data)
+    {
+        if (head == nullptr)
+        {
+            Node *n = new Node(data);
+            head = tail = n;
+        }
+        else
+        {
+            Node *n = new Node(data);
+            tail->next = n;
             tail = n;
         }
     }
-    //Insert at a Position
-    void Insert(int data, int pos){
-        if (pos==0)
+    // Insert at a Position
+    void Insert(int data, int pos)
+    {
+        if (pos == 0)
         {
             push_front(data);
             return;
         }
-        else{
-            Node* temp = head; //start from head
-            for (int i = 1; i <= pos-1; i++)
+        else
+        {
+            Node *temp = head; // start from head
+            for (int i = 1; i <= pos - 1; i++)
             {
                 temp = temp->next;
             }
-            
-            Node * n = new Node(data);
+
+            Node *n = new Node(data);
             n->next = temp->next;
             temp->next = n;
-            
-        }   
+        }
     }
 
-    //normal search
-    int Search(int k){
-        Node * tmp = head;
+    // normal search
+    int Search(int k)
+    {
+        Node *tmp = head;
         int idx = 0;
-        //iterate 
-        while (tmp!=NULL){
-            if(tmp->data==k){
+        // iterate
+        while (tmp != NULL)
+        {
+            if (tmp->data == k)
+            {
                 return idx;
             }
             idx++;
             tmp = tmp->next;
         }
         return -1;
-        
-
     }
-    //Recursive Search
-    int recursiveSearch(int k){
+    // Recursive Search
+    int recursiveSearch(int k)
+    {
         int idx = searchHelper(head, k);
-        return idx;  
-        }
+        return idx;
+    }
 
-    //POP front
+    // POP front
 
-    void Pop_Front(){
+    void Pop_Front()
+    {
         Node *tmp = head;
         head = head->next;
-         tmp->next = NULL;
-         delete tmp;
+        tmp->next = NULL;
+        delete tmp;
     }
-
-    
-
-
-
-
-
-
 };
--- C++ Practice Ground/Everyday Practice/Practice.cpp
+++ C++ Practice Ground/Everyday Practice/Practice.cpp
@@ -6,31 +6,23 @@ bool compare(int a, int b)
 {
     return a > pow(b, 2);
 }
-void Display(vector<int> &arr)
+void Display(map<int, int> &arr)
 {
     for (auto x : arr)
     {
-        cout << x << ",";
+        cout << x.first << " " << x.second << endl;
     }
 }
 int main()
 {
-    vector<int> arr;
+    map<int, int> arr;
 
-    arr.push_back(23);
-    arr.push_back(24);
-    arr.push_back(25);
-    arr.push_back(26);
-    arr.push_back(27);
-    arr.push_back(28);
-    arr.push_back(29);
-    arr.pop_back();
-    arr.insert(arr.begin() + 3, 45);
-    cout << arr.size() << endl;
-    cout << accumulate(arr.begin(), arr.end(), 0) << endl;
-    sort(arr.begin(), arr.end(), compare);
+    arr.insert({1, 15});
+    arr.insert({4, 25});
+    arr.insert({2, 23});
+    arr.insert({3, 24});
+    arr.insert({5, 26});
 
     Display(arr);
-
     return 0;
 }
