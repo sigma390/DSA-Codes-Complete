5e5b134e1bd3b1b58125806935258290c0f075d3
🕉️ Infinite <git21cs072-t@students.git.edu>
2025-07-26 18:25:57 +0530

    kadane optimised working

--- NeetCode/Standard Questions/Arrays/Kadane.cpp
+++ NeetCode/Standard Questions/Arrays/Kadane.cpp
@@ -47,17 +47,36 @@ vector<int> BF_MAX_subarray(int *arr, int n)
     return subarray;
 }
 
+// optimised O(N)
+
+int kadaneOptimised(int *arr, int n)
+{
+    int maxSum = INT_MIN;
+
+    int currSum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        currSum = max(currSum, 0);
+        currSum += arr[i];
+        maxSum = max(maxSum, currSum);
+    }
+    return maxSum;
+}
+
 int main()
 {
     int arr[] = {
         4, -1, 2, -7, 3, 4
 
     };
-    cout << "The max Sum of Given Array is:" << BruteFORCE(arr, 6);
+    cout << "The max Sum of Given Array is: " << BruteFORCE(arr, 6);
     vector<int> result = BF_MAX_subarray(arr, 6);
     cout << "The max subarray is:";
     for (int i = 0; i < result.size(); i++)
     {
         cout << result[i] << ", ";
     }
+
+    cout << "\n This is max Subarray Sum otimised Version : " << kadaneOptimised(arr, 6);
 }
