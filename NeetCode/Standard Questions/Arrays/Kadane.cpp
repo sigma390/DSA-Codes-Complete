bc30453e94b5227e3886a9fc0a233662e982da24
🕉️ Infinite <git21cs072-t@students.git.edu>
2025-07-26 09:38:08 +0530

    Kadane Max Subarray BruteForce Done

--- NeetCode/Standard Questions/Arrays/Kadane.cpp
+++ NeetCode/Standard Questions/Arrays/Kadane.cpp
@@ -17,6 +17,36 @@ int BruteFORCE(int *arr, int n)
     return (int)maxSum;
 }
 
+// Maximum SubARRAY
+vector<int> BF_MAX_subarray(int *arr, int n)
+{
+    long long maxSum = INT_MIN;
+    int start = 0;
+    int end = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int currSum = 0;
+        for (int j = i; j < n; j++)
+        {
+            currSum += arr[j];
+            // Compare and get Indices
+            if (currSum > maxSum)
+            {
+                maxSum = currSum;
+                start = i;
+                end = j;
+            }
+        }
+    }
+    vector<int> subarray;
+    for (int i = start; i <= end; i++)
+    {
+        subarray.push_back(arr[i]);
+    }
+
+    return subarray;
+}
+
 int main()
 {
     int arr[] = {
@@ -24,4 +54,10 @@ int main()
 
     };
     cout << "The max Sum of Given Array is:" << BruteFORCE(arr, 6);
+    vector<int> result = BF_MAX_subarray(arr, 6);
+    cout << "The max subarray is:";
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << ", ";
+    }
 }
