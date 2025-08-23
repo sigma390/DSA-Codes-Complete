1c10f9a39f6e62a618d68ffaac7cf486809f3d8e
🕉️ Infinite <sigmacoder390@gmail.com>
2025-08-23 09:29:19 +0530

    went THROUGH vectors , grids !!

--- Daily Ritual/AllTest.cpp
+++ Daily Ritual/AllTest.cpp
@@ -4,33 +4,15 @@ using namespace std;
 int main()
 {
 
-    set<int> s;
-    s.insert(10);
-    s.insert(20);
-    s.insert(30);
-    s.insert(40);
-    s.insert(11);
-    s.insert(22);
-    s.insert(33);
-    s.insert(44);
-    for (auto x : s)
-    {
-        cout << x << ",";
-    }
-    set<int> st;
-    vector<int> arr{23, 34, 678, 1};
-
+    vector<vector<int>> arr(3, vector<int>(10, 0)); // meaning
+    // there will be 3 rows and each Row is A Vector with 10 elements Initiated To 0
     for (auto x : arr)
     {
-        st.insert(x);
+        for (auto y : x)
+        {
+            cout << y << ",";
+        }
     }
-    cout << "\nthis is Set Formed out Of a Vector" << endl;
-    for (auto x : st)
-    {
-        cout << x << ",";
-    }
-    cout << ((st.find(678) != st.end()) ? "Found\n" : "not Found\n") << endl;
-    cout << st.count(6) << endl;
-    cout << st.count(678) << endl;
+
     return 0;
 }
--- Daily Ritual/Vectors.cpp
+++ Daily Ritual/Vectors.cpp
@@ -3,19 +3,46 @@ using namespace std;
 
 int main()
 {
-    vector<int> arr{11, 12, 13};
-    arr.push_back(345);
+    vector<int> one_d_array{11, 12, 13};
 
-    arr.push_back(890);
+    //============> 2D Vector Initialisation <================
+    /*there will be 3 rows and each Row is A Vector with 10 elements Initiated To 0
+     Iterating Through the Grid */
+    vector<vector<int>> arr(3, vector<int>(10, 0));
 
-    cout << arr.size() << "this is Old Size : " << endl;
-    arr.resize(10);
-    cout << arr.size() << "this is New Size : " << endl;
+    //=============> Grid initialisation and iterating over a Grid <==========
+    int rows = 4;
+    int cols = 6;
+    int initialval = 5;
+    vector<vector<int>> grid(rows, vector<int>(cols, initialval));
+    for (int row = 0; row < grid.size(); row++)
+    {
+        for (int col = 0; col < grid[row].size(); col++)
+        {
+            cout << grid[row][col] << ",";
+        }
+        cout << endl;
+    }
 
-    // // arr.insert(arr.begin() + 3, 89);
-    // for (auto x : arr)
-    // {
-    //     cout << x << ",";
-    // }
+    //===================> Normal One D array <========================
+    vector<int> arrr{10, 20, 30};
+    arrr.push_back(100);
+    arrr.push_back(200);
+    arrr.push_back(300);
+    arrr.pop_back();
+    arrr.insert(arrr.begin() + 3, 777);
+    arrr.erase(arrr.begin() + 4); // particular Element Remove Krna
+    int Sum = 0;
+    Sum = accumulate(arrr.begin(), arrr.end(), 10); // 10 is initial value of The Variable that we are Going to Accumulate
+
+    for (auto x : arrr)
+    {
+        cout << x << ",";
+    }
+    for (int i = 0; i < arrr.size(); i++)
+    {
+        cout << arrr[i] << ",";
+    }
+
     return 0;
 }
